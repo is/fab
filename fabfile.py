7e71b3b87ddb0ba47819f7bc9573ec8d040c6d05
Yu Xin <scaner@gmail.com>
2012-10-03 19:02:01 +0800

    New & smarter run_sh function.

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,50 @@
-import time, csv
+import os, sys
+import time
+
 from fabric.api import *
 
 import hostsdb
 
 import ops
 
-
+@task
 def verify_hostname():
+	"""Verify host up & output hostname"""
 	run ('hostname')
 
-@task(alias='R')
-def run_sh(cmd):
+
+@task
+def run_sh_oneline(cmd):
+	run(cmd)
+
+@task(alias='R__')
+def run_sh_script(cmd):
 	'''Run shell script on remote machines by SSH'''
 	fn = '/tmp/%s_%s' % (cmd.replace('/', '__'), int(time.time()))
 	put (cmd, fn, mode = 0700)
 	run (fn)
 
-# vim:ts=2
+@task(alias='R')
+def run_sh(*argv):
+	if len(argv) == 0:
+		cmd = None
+	else:
+		cmd = argv[0]
+
+	if cmd == None:
+		fn = "/tmp/run_sh_%s__%s" % (
+			time.strftime('%Y%m%d-%H%M%S'), 
+			os.getpid())
+
+		fout = open(fn, 'w')
+		fout.write(sys.stdin.read())
+		fout.close()
+		execute(run_sh_script, fn)
+		# Read Script from standard input
+	elif cmd.startswith('>>>'):
+		execute(run_sh_oneline, cmd[3:])
+	else:
+		execute(run_sh_script, cmd)
+		
+		
+# vim:ts=2 ai
