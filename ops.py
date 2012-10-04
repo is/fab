f4415c5a135eb7dcb9d049199f829ff8b77cf06c
Yu Xin <scaner@gmail.com>
2012-10-04 14:08:37 +0800

    rsync_yum_conf in ops

--- ops.py
+++ ops.py
@@ -4,4 +4,8 @@ from fabric.api import *
 def rsync_yum_repos():
 	local('rsync -e ssh -av --delete /etc/yum.repos.d %s:/etc' % env.host)	
 
+@task
+def rsync_yum_conf():
+	local('rsync -e ssh -av --delete /etc/yum.conf %s:/etc' % env.host)	
+
 # vim:ts=2 
